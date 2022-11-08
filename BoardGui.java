f30b4e4af5719d86869d4b4eb5409d22bd614ebf
Elad Joseph <eladj2002@gmail.com>
2022-11-08 19:08:20 +0200

    improved vesion

--- BoardGui.java
+++ BoardGui.java
@@ -12,15 +12,33 @@ public class BoardGui{
     JButton b8;
     JButton b9;
     String player;
-
+    int[][] board;
+    int[] boardX;
+    int[] boardY;
+    int[] results;
+    JFrame window;
+    boolean flag;
+    int counter;
     public void main(){
         int boardHight = 800;
         int boardWidth = 800;
         int buttonHight = 100;
         int buttonWidth = 100;
+        board = new int[3][3];
+        boardX = new int[600];
+        boardY = new int[600];
+        results = new int[9];
         player = "x"; //which player turn it is
+        flag = true;
+        counter = 0;
+        for(int x = 0; x < 3; x++){
+            for(int y = 0; y < 3; y++){
+                board[x][y] = 0;
+            }
+        }
+    
 
-        JFrame window = new JFrame(); // craeting screen
+        window = new JFrame(); // craeting screen
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         window.setLayout(null);
         window.setSize(boardWidth,boardHight);
@@ -58,7 +76,14 @@ public class BoardGui{
         b7.setBounds(200,550,buttonWidth,buttonHight);
         b8.setBounds(350,550,buttonWidth,buttonHight);
         b9.setBounds(500,550,buttonWidth,buttonHight);
-        
+        boardX[200] = 0;
+        boardX[350] = 1;
+        boardX[500] = 2;
+
+        boardY[150] = 0;
+        boardY[350] = 1;
+        boardY[550] = 2;
+
         AddMouseListener(b1);
         AddMouseListener(b2);
         AddMouseListener(b3);
@@ -68,36 +93,94 @@ public class BoardGui{
         AddMouseListener(b7);
         AddMouseListener(b8);
         AddMouseListener(b9);
-
+        
         window.setVisible(true);
     }
+    public void createButton(int x, int y){
+        var button = new JButton("");
 
+    }
     public void AddMouseListener(JButton Button) {
         Button.addMouseListener(new java.awt.event.MouseAdapter()
         {
-            public void mouseClicked(java.awt.event.MouseEvent evt)
+            public void mousePressed(java.awt.event.MouseEvent evt)
             {
                 MouseClicked(Button);
+                printboard();
             }
         });
     }
-
     public void MouseClicked(JButton button) 
     {
         if(button.getText() != "")
             return;
+
+        
+        results[8] ++;
+
+        var rec = button.getBounds();  
+        
+        
         button.setText(player);
         if(player == "x")
+        {
             player = "o";
-        else if(player == "o")
-            player = "x";
+            addToResults(rec.x, rec.y, 3);
         }
-    public void CheckWin(){
-
-    }        
+        else if(player == "o")
+        {
+            player = "x";
+            addToResults(rec.x, rec.y, 4);
+        }
+        CheckWin();
+    }
+    public void addToResults(int x,int y,int value){
+        board[boardY[y]][boardX[x]] = value;
+        results[boardY[y] +1] += value;
+        results[7 - boardX[x]] += value;
+        if (boardX[x]+boardY[y] == 2)
+            results[0] += value;
+        if (boardX[x] == boardY[y])
+            results[4] += value;
+    }
+    public void CheckWin()
+    {
+        for(int i : results){
+            if (i == 12){
+                JOptionPane.showMessageDialog(null, "player O wins");
+                window.dispose();
+                main();
+                break;
+                
+            }
+            else if (i == 9){
+                JOptionPane.showMessageDialog(null, "player X wins");
+                window.dispose();
+                main();
+                break;
+                
+            }     
+        }
+        if (results[8] == 9){
+            JOptionPane.showMessageDialog(null, "no one won");
+            window.dispose();
+            main();
+        }
+    }
+    public void printboard()
+    {
+        for(int i = 0;i<3;i++){
+            for(int j = 0;j<3;j++)
+                System.out.print(board[i][j]);
+            System.out.println();
+        }
+        System.out.println();
+    }
 } 
-/*
- * (0,0)(0,1)(0,2)
- * (1,0)(1,1)(1,2)
- * (2,0)(2,1)(2,2)
+/*                (0)
+ * (0,0)(0,1)(0,2)(1)
+ * (1,0)(1,1)(1,2)(2)
+ * (2,0)(2,1)(2,2)(3)
+ *(7)   (6)  (5)  (4)
+ *(8) counter
  */
--- BoardGuiTest.java
+++ BoardGuiTest.java
@@ -1,6 +1,6 @@
 public class BoardGuiTest {
-    public static void main(String[] args){
-    BoardGui boardgui = new BoardGui();
-    boardgui.main();
+    public static void main(String[] args)throws InterruptedException{
+        BoardGui boardgui = new BoardGui();
+        boardgui.main();
     }
 }
